2819246f8e507d45fe91765a1b80532863750339
Andy Dobbs <andrew.dobbs@gmail.com>
2023-03-21 09:59:39 -0400

    Add assets, styling, and new content

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,10 +1,14 @@
+import Head from "next/head";
 import Link from "next/link";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
     return (
-        <>
-            <Link href="/">Back to home</Link>
+        <Layout>
+            <Head>
+                <title>First Post</title>
+            </Head>
             <h1>First Post</h1>
-        </>
+        </Layout>
     );
 }
